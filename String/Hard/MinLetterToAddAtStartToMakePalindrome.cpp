89a7188c0bb7d43f41451862ec909548727f61cd
Mitansh Patel <mitanshpatel288@gmail.com>
2024-01-21 16:55:21 +0530

    21-01-2024

---
+++ String/Hard/MinLetterToAddAtStartToMakePalindrome.cpp
@@ -0,0 +1,71 @@
+// Given string str of length N. The task is to find the minimum characters to be added at the front to make string palindrome.
+// Note: A palindrome is a word which reads the same backward as forward. Example: "madam".
+
+// Example 1:
+
+// Input:
+// S = "abc"
+// Output: 2
+// Explanation: 
+// Add 'b' and 'c' at front of above string to make it
+// palindrome : "cbabc"
+
+#include <bits/stdc++.h> 
+using namespace std;
+
+vector<int> computeLPSArray(string str){
+    int M = str.length();
+    vector<int> lps(M);
+ 
+    int len = 0;
+    lps[0] = 0; // lps[0] is always 0
+ 
+    // the loop calculates lps[i] for i = 1 to M-1
+    int i = 1;
+    while (i < M)
+    {
+        if (str[i] == str[len])
+        {
+            len++;
+            lps[i] = len;
+            i++;
+        }
+        else // (str[i] != str[len])
+        {
+            // This is tricky. Consider the example.
+            // AAACAAAA and i = 7. The idea is similar
+            // to search step.
+            if (len != 0)
+            {
+                len = lps[len-1];
+ 
+                // Also, note that we do not increment
+                // i here
+            }
+            else // if (len == 0)
+            {
+                lps[i] = 0;
+                i++;
+            }
+        }
+    }
+    return lps;
+}
+ 
+    
+int minChar(string str){
+    //Write your code here
+    string revStr = str;
+    reverse(revStr.begin(), revStr.end());
+
+    // Get concatenation of string, special character
+    // and reverse string
+    string concat = str + "$" + revStr;
+
+    //  Get LPS array of this concatenated string
+    vector<int> lps = computeLPSArray(concat);
+
+    // By subtracting last entry of lps vector from
+    // string length, we will get our result
+    return (str.length() - lps.back());
+}
